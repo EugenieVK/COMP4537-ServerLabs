ad388676fa6d815177ca47f4899cc35800161843
Eugenie Kim <eugenie.kim@outlook.com>
2025-02-01 18:38:26 -0800

    Adding code so server can be editted for hosting more labs

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ const url = require("url");
 
 const fileName = "file.txt";
 
+const lab3Pages = {
+    "datePage" : "getDate",
+    "writePage": "writeFile",
+    "readPage" : "readFile",
+    "param" : "3"
+}
 
 http.createServer((req, res) => {
     const urlParams = req.url.split("/");
@@ -15,26 +21,31 @@ http.createServer((req, res) => {
     }
 
     res.writeHead(200, { 'Content-Type': 'text/html' });
-
-    const editor = new FileEditor(fileName);
-    switch (urlParams[1]) {
-        case "getDate":
-            const name = url.parse(req.url, true).query.name;
-            const greeter = new Greeter(name);
-            res.write(greeter.getGreeting());
-            break;
-        case "writeFile":
-            const text = url.parse(req.url, true).query.text;
-            res.write(editor.writeToFile(text));
-            break;
-        case "readFile":
-            const file = urlParams[2];
-            res.write(editor.readFromFile(file));
-            break;
-        default:
-            const page404 = `<h2 style="color:red">${messages.strings.page404}</h2>`;
-            res.write(page404);
+    if(urlParams[1] == lab3Pages.param){
+        const editor = new FileEditor(fileName);
+        switch (urlParams[1]) {
+            case lab3Pages.datePage:
+                const name = url.parse(req.url, true).query.name;
+                const greeter = new Greeter(name);
+                res.write(greeter.getGreeting());
+                break;
+            case lab3Pages.writePage:
+                const text = url.parse(req.url, true).query.text;
+                res.write(editor.writeToFile(text));
+                break;
+            case lab3Pages.readPage:
+                const file = urlParams[2];
+                res.write(editor.readFromFile(file));
+                break;
+            default:
+                const page404 = `<h2 style="color:red">${messages.strings.page404}</h2>`;
+                res.write(page404);
+        }
+    } else {
+        const page404 = `<h2 style="color:red">${messages.strings.page404}</h2>`;
+        res.write(page404);
     }
+    
     res.end();
 
 
